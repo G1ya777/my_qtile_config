7d06b3ac7af47a72c04b26ec5ec3b3a9e57cbf2f
G1ya777 <g1ya777@protonmail.com>
2023-06-23 17:45:02 +0100

    first commit

---
+++ autostart.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+xrandr --output DP-0 --mode 1920x1080 --rate 144 &
+redshift -x && redshift -O 4000 &
+picom --config  ~/.config/picom/picom.conf &
+nitrogen --restore &
+lxpolkit &
+caffeine &
+parcellite &
+pasystray &
+xpad &
+muezzin &
+uget-gtk &
